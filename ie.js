707ec2a4e2f33338965c0bc6807aa06775ac7155
Renato Gama <renatoargh@gmail.com>
2013-05-16 00:13:34 -0300

    Algoritmo de cálculo do Mod11 - fazer testes

---
+++ ie.js
@@ -0,0 +1,29 @@
+var estados = 
+	["ac", "al", "ap", "am",
+	 "ba", "ce", "df", "es",
+	 "go", "ma", "mt", "ms",
+	 "mg", "pa", "pb", "pr",
+	 "pe", "pi", "rj", "rn",
+	 "rs", "ro", "rr", "sc",
+	 "sp", "se", "to"];
+
+function validar(estado, ie){
+	if(typeof estado === "undefined") throw new Error("Estado deve ser forncedido");
+	if(estados.indexOf(estado.toLowerCase()) === -1) throw new Error("O estado fornecido não é válido"); 
+	if(typeof ie === "undefined") throw new Error("Inscrição estadual deve ser forncedida");
+	
+	
+}
+
+function mod11(valor, retornarResto, multiplicadores){
+	if(typeof retornarResto === "undefined") retornarResto = false;
+	if(typeof multiplicadores === "undefined") multiplicadores = [2, 3, 4, 5, 6, 7, 8, 9];
+		
+	var i = 0;
+	var resto = valor.split("").reduceRight(function(anterior, atual){
+		if(i > multiplicadores.length - 1) i = 0;
+		return (multiplicadores[i++] * parseInt(atual)) + anterior; 
+	}, 0) % 11;
+	
+	return retornarResto ? resto : (11 - resto >= 10 ? 0 : 11 - resto);
+}
