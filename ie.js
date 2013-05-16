fe104f895e55a56cb4aa1d7dafaa956c27a80e68
Renato Gama <renatoargh@gmail.com>
2013-05-16 13:05:20 -0300

    Implementado função de validação do DF e do GO

--- ie.js
+++ ie.js
@@ -10,13 +10,62 @@ var estados =
 function validar(estado, ie){
 	if(typeof estado === "undefined") throw new Error("Estado deve ser forncedido");
 	if(estados.indexOf(estado.toLowerCase()) === -1) throw new Error("O estado fornecido não é válido"); 
-	if(typeof ie === "undefined") throw new Error("Inscrição estadual deve ser forncedida");
+	if(typeof ie === "undefined") throw new Error("Inscrição estadual deve ser fornecida");
+	if(typeof ie !== "string") throw new Error("Inscrição estadual deve ser um string");
 	
+	return this[estado](ie);
+};
+
+function go(valor){
+	if(!/^\d+$/.test(valor)) return false;
+	if(valor.length !== 9) return false;
+	if(["10", "11", "15"].indexOf(valor.substring(0, 2)) === -1) return false;
 	
+	var base = valor.substring(0, 8);
+	
+	if(base === "11094402"){
+		if(valor.substr(8) === "1" || valor.substr(8) === "0")
+			return true;
+		else
+			return false;
+	}
+	
+	var resto = mod11(base);
+	
+	var digito = null;
+	if(resto === 0) digito = 0;
+	else if(resto === 1){
+		if(entre(base, 10103105, 10119997)) digito = 1;
+		else digito = 0;
+	}
+	else digito = 11 - resto; 
+
+	return valor === base + digito;
+}
+
+function df(valor){
+	if(!/^\d+$/.test(valor)) return false;
+	if(valor.length !== 13) return false;
+	
+	var base = valor.substring(0, 11);
+
+	var restoPrimeiro = mod11(base);
+	var primeiro = 11 - restoPrimeiro >= 10 ? 0 : 11 - restoPrimeiro;
+	
+	var restoSegundo = mod11(base + primeiro);
+	var segundo = 11 - restoSegundo >= 10 ? 0 : 11 - restoSegundo;
+	
+	return valor === base + primeiro + segundo;
+}
+
+function entre(valor, limiteInferior, limiteSuperior){
+	if(typeof valor === "string") valor = parseInt(valor);
+	return valor >= limiteInferior && valor <= limiteSuperior;
 }
 
 function mod11(valor, multiplicadores){
-	if(typeof multiplicadores === "undefined") multiplicadores = [2, 3, 4, 5, 6, 7, 8, 9];
+	if(typeof multiplicadores === "undefined")
+		multiplicadores = [2, 3, 4, 5, 6, 7, 8, 9];
 		
 	var i = 0;
 	return valor.split("").reduceRight(function(anterior, atual){
