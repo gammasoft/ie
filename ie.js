3ec23fb8d26df85d24a252ad0ac021916852403f
Joao Tonial <joao.tonial@sbsistemas.com.br>
2019-04-12 14:02:45 -0300

    Suporte para codigo do IBGE
    Adicioando suporte para codigo de estado do IBGE tendo em vista que muitos desenvolvedores o utilizam  como referencia para os estados

--- ie.js
+++ ie.js
@@ -551,7 +551,44 @@ function lookup(ie){
     }
 }
 
+var codigosIBGE = {
+    11:	'RO',
+    13:	'AM',
+    12:	'AC',
+    14:	'RR',
+    15:	'PA',
+    16:	'AP',
+    17:	'TO',
+    21:	'MA',
+    22:	'PI',
+    23:	'CE',
+    24:	'RN',
+    25:	'PB',
+    26:	'PE',
+    27:	'AL',
+    28:	'SE',
+    29:	'BA',
+    31:	'MG',
+    32: 'ES',
+    33: 'RJ',
+    35:	'SP',
+    41:	'PR',
+    42:	'SC',
+    43:	'RS',
+    50:	'MS',
+    51:	'MT',
+    52:	'GO',
+    53:	'DF',
+};
+
 function validar(ie, estado){
+    if(estado && !isNaN(estado)) {
+        if(!Object.keys(codigosIBGE).includes(estado)){
+            throw new Error('código não é valido');
+        }
+        estado = codigosIBGE[estado];
+    }
+
     if(eIndefinido(estado) || estado === null){
         estado = '';
     }
